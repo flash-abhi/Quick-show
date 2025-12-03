7002e7feefb1a0d97d5c4b8d1fc4cc5ac13d7d9c
Abhishek <abhishekchauhan9119001283@gmail.com>
2025-12-03 19:44:49 +0530

    movie Detail page is completed

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { assets } from '../assets/assets'
 
 const Footer = () => {
@@ -6,7 +5,7 @@ const Footer = () => {
     <footer className="px-6 md:px-16 lg:px-36 mt-40 w-full text-gray-300">
             <div className="flex flex-col md:flex-row justify-between w-full gap-10 border-b border-gray-500 pb-14">
                 <div className="md:max-w-96">
-                    <img class="w-36 h-auto" src={assets.logo} alt="logo"/>
+                    <img className="w-36 h-auto" src={assets.logo} alt="logo"/>
                     <p className="mt-6 text-sm">
                       Quick Show is your go-to destination for movie tickets, latest releases, and a seamless booking experience. Discover new films, explore nearby theaters, and secure your seats in just a few clicks. We aim to make every movie night smoother, faster, and more enjoyable.
                     </p>
--- frontend/src/pages/SeatLayout.jsx
+++ frontend/src/pages/SeatLayout.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const SeatLayout = () => {
+  const {id,date} = useParams();
+  const [selectedSeats,setSelectedSeats] = useState([]);
+  const [selectedTime,setSelectedTime] = useState(null);
+  const [show,setShow] = useState(null);
+  const navigate = useNavigate();
   return (
-    <div>SeatLayout</div>
+    <div>
+      
+    </div>
   )
 }
 
